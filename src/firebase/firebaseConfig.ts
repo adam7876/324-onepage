492cebd0e9d08e93c5a4c9ef6d89a1e2879be763
adam7876 <adam7876@users.noreply.github.com>
2025-06-26 05:14:56 +0900

    chore: firebaseConfig 改用 .env.local，提升安全性

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -5,13 +5,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyBdvXhGQiyFaH7buX4ILyo-tVjPrbtMXQg",
-  authDomain: "one-page-bec86.firebaseapp.com",
-  projectId: "one-page-bec86",
-  storageBucket: "one-page-bec86.firebasestorage.app",
-  messagingSenderId: "954851339318",
-  appId: "1:954851339318:web:d6f5fd1ced342120e83c97",
-  measurementId: "G-4JSW7ELRC2"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
